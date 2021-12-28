74ec5733ec3e54c05f73d1bd3d6dac5ec7620df0
Ryan Duffy <ryan@replay.io>
2021-12-28 13:31:04 -0600

    fix build-test-branch script to support branches with forward slash

--- .github/actions/build-test-branch/main.js
+++ .github/actions/build-test-branch/main.js
@@ -6,7 +6,7 @@ const {
   newTask,
 } = require("../utils");
 
-const branchName = getBranchName(process.env.GITHUB_REF);
+const branchName = process.env.GITHUB_REF_NAME;
 console.log("BranchName", branchName);
 
 if (branchName.includes("webreplay-release")) {
@@ -54,13 +54,4 @@ function platformTasks(platform) {
   );
 
   return [buildReplayTask, testReplayTask];
-}
-
-function getBranchName(refName) {
-  // Strip everything after the last "/" from the ref to get the branch name.
-  const index = refName.lastIndexOf("/");
-  if (index == -1) {
-    return refName;
-  }
-  return refName.substring(index + 1);
 }
