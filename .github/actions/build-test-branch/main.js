960c7dd7f421daf66a258ada51a79d3dc753ae0e
Brian Hackett <bhackett1024@gmail.com>
2022-01-20 08:37:08 -0800

    Add clobber input to build/test branch action (#663)

--- .github/actions/build-test-branch/main.js
+++ .github/actions/build-test-branch/main.js
@@ -19,6 +19,13 @@ const replayRevision = getLatestReplayRevision();
 const driverRevision = process.env.INPUT_DRIVER_REVISION;
 console.log("DriverRevision", driverRevision);
 
+const clobberInput = process.env.INPUT_CLOBBER;
+console.log("Clobber", clobberInput);
+const clobber = clobberInput == "true";
+
+const buildOnly = !!process.env.BUILD_ONLY;
+console.log("BuildOnly", buildOnly);
+
 sendBuildTestRequest({
   name: `Gecko Build/Test Branch ${branchName} ${replayRevision}${driverRevision ? " driver " + driverRevision : ""}`,
   tasks: [
@@ -37,6 +44,7 @@ function platformTasks(platform) {
       revision: replayRevision,
       branch: branchName,
       driverRevision,
+      clobber,
     },
     platform
   );
