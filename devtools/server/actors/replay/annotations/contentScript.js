d8bf9ae515f37de2710abc60362d40ca368c2ea8
Ryan Duffy <ryan@replay.io>
2022-10-31 16:06:30 -0700

    rename annotation; add input validation

--- devtools/server/actors/replay/annotations/contentScript.js
+++ devtools/server/actors/replay/annotations/contentScript.js
@@ -10,8 +10,16 @@ function initialize(dbgWindow, RecordReplayControl) {
 
   window.wrappedJSObject.__RECORD_REPLAY_ANNOTATION_HOOK__ =
     (source, message) => {
+      if (!source || typeof source !== "string") {
+        throw new Error("Replay annotations must include a source");
+      }
+
+      if (message && typeof message !== "object") {
+        throw new Error("Replay annotation messages must be an object if set");
+      }
+
       RecordReplayControl.onAnnotation(
-        "generic-annotation",
+        "generic",
         JSON.stringify({
           source,
           message
