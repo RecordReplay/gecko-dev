9a0e428a83ee78c7596af4fdc87719ae68fbd917
Dan Miller <dan@replay.io>
2023-05-18 18:25:26 -0700

    print stuff

--- windows-build.sh
+++ windows-build.sh
@@ -3,5 +3,7 @@ set -euox pipefail
 cd recordreplay
 cd $GECKODIR
 ./mach build
+echo "Exit status: $?"
 ./mach package
+echo "Exit status: $?"
 MOZ_PRODUCT_VERSION=86.0 MAR_CHANNEL_ID=release MAR=rr-opt/dist/host/bin/mar ./tools/update-packaging/make_full_update.sh rr-opt/replay.complete.mar rr-opt/dist/replay
